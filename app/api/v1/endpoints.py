da7d83ca5c96c723fa09c79b4c7a2cea2614fde6
brichardson2x <brichardson2x@gmail.com>
2025-09-03 12:43:37 -0500

    middlewarenon ansync

--- app/api/v1/endpoints.py
+++ app/api/v1/endpoints.py
@@ -18,7 +18,7 @@ logger = setup_logging()
 limiter = Limiter(key_func=get_remote_address)
 router = APIRouter(dependencies=[Depends(read_security)])
 
-async def add_middleware(app):
+def add_middleware(app):
     app.state.limiter = limiter
     app.add_exception_handler(RateLimitExceeded, lambda _: JSONResponse(
         status_code=429, content={"message": "Too many requests, slow down!"}
